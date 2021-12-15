caeb818e55f4dfe97fcb5a6426fa7bb525127dba
Marion Pluyaud <marion070189@hotmail.fr>
2021-12-15 16:20:43 +0100

    paramétrage multer sur route createPost

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
 const postController = require('../controllers/post');
-
+const multer = require('../middleware/multer-config');
 const router = express.Router();
 
-router.post('/', postController.createPost);
+router.post('/', multer, postController.createPost);
 router.get('/', postController.getAllPost);
 
 module.exports = router;
