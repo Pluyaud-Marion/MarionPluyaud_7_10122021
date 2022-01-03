1261360817021808250a74c60c9a912ef8b7232d
Marion Pluyaud <marion070189@hotmail.fr>
2022-01-03 10:56:59 +0100

    changement timezone pour db

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,13 +1,15 @@
-const dotenv = require('dotenv').config();
+// eslint-disable-next-line no-unused-vars
+const dotenv = require("dotenv").config();
 
 module.exports = {
   
-  development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: 'mysql'
-  }
+	development: {
+		username: process.env.DB_USERNAME,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_DATABASE,
+		host: process.env.DB_HOST,
+		dialect: "mysql",
+		timezone: "+1:00"
+	}
 };
 
